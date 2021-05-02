65ee75c9a69899e3b4af9ae5442db49ea6791369
jeffIrvine <jirvine1212@gmail.com>
2021-05-01 17:44:49 -0700

    test two passing

--- bc2-code-challenges/src/01-for-each/01-for-each.js
+++ bc2-code-challenges/src/01-for-each/01-for-each.js
@@ -33,11 +33,14 @@ Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
 export const addValues = (arr, value) => {
-    // Solution code here...
+    arr.push(value)
 };
 
 export const addNumbers = (num, arr, times, callback) => {
-    // Solution code here...
+    for(let i = 0; i < times; i++){
+        callback(arr, num)
+    }
+    return arr
 };
 
 /* ------------------------------------------------------------------------------------------------
